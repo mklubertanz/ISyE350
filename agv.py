e165cc892b428a10d96011f5abdb9e7d3f7876bf
niemuth2 <45105392+niemuth2@users.noreply.github.com>
2018-12-04 14:36:36 -0600

    Update agv.py

--- agv.py
+++ agv.py
@@ -1,7 +1,5 @@
-
 from ev3dev.ev3 import *
 #added this ^ SK
-import time
 from time import sleep
 '''
 The agv function will act as the "main" method for your system. dairy_land is a Factory object from the factory.py file.
@@ -13,23 +11,23 @@ IMPORTANT: each time you call the read_order() function, the order at the front 
 read_order() function until the previous order has been fulfilled.
 '''
 def agv(dairy_land):
-    
+
     #Initialize all of your objects before the while loop
-    ars = ARS(0,0) #ARS will start at (0,0) on the coordinate plain every time
-    # TODO verify coordinates
-    r1 = Bin('red', 'r1', 3, 5)
-    r2 = Bin('red', 'r2', 1, 3)
-    r3 = Bin('red', 'r3', 3, 1)
+    ars = ARS(1,1) #ARS will start at (0,0) on the coordinate plain every time
+    r1 = Bin('red', 'r1', 1, 1)
+    r2 = Bin('red', 'r2', 3, 1)
+    r3 = Bin('red', 'r3', 3, 3)
     y1 = Bin('yellow', 'y1', 1, 5)
-    y2 = Bin('yellow', 'y2', 3, 3)
-    b1 = Bin('blue','b1', 1, 1)
-    bins = [r1, r2, r3, y1, y1, b1, b2]
+    y2 = Bin('yellow', 'y2', 1, 3)
+    b1 = Bin('blue','b1', 3, 5)
+    bins = [r1, r2, r3, y1, y2, b1]
 
     #TODO add packer coordinates
-    p1 = Packer(1, 0, 0)
-    p2 = Packer(2, 0, 6)
-    p3 = Packer(3, 4, 6)
-    
+    p1 = Packer(1, 0, 7)
+    p2 = Packer(2, 5, 7)
+    p3 = Packer(3, 0, 0)
+    packers = [p1, p2, p3]
+
     while not dairy_land.test_complete:
         # read an order:
         cur_order = dairy_land.read_order()
@@ -37,113 +35,80 @@ def agv(dairy_land):
         if cur_order is not None:
             # fulfill the order:
             # bins --> an array that stores all of the bin objects you've created
-            cur_bin = ars.find_nearest_bin(bins, cur_order.packer, cur_order.color)
+            cur_bin = ars.find_nearest_bin(bins, packers[cur_order.packer - 1], cur_order.color)
             # cur_bin --> the closest bin returned from the find_nearest_bin function
-            print(cur_bin.name) # SK test 
+            print(cur_bin.name) # SK test
             # Move robot to closest bin:
-            move_to_bin(cur_bin)
+            ars.move_to_bin(cur_bin)
             # Pause while cheese is loaded:
-            #TODO add method that makes ARS sleep for a few seconds
-            
+            sleep(3)
             # Move to correct packer:
-            move_to_packer(cur_bin, cur_order.packer)          
+            ars.move_to_packer(cur_bin, packers[cur_order.packer - 1])
+            print(packers[cur_order.packer - 1].region) # SK test
             # Move bin to open (and optimal) spot
-            move_to_bin(cur_bin) #for now, assume we're taking bin back to its location
-            '''
-            Begin ARS logic here. Again, this will serve as your main method. After you read an order in the queue,
-            your first step should be to determine which packer requires the order and for which type of cheese the
-            order is for.
-            Each Order object has the following attributes:
-            packer: an integer (1, 2 or 3) representing which packer the order is for
-            color: a string ('red', 'yellow' or 'blue') representing the color bin to retrieve for the order
-            See Order class in factory.py for more details...
-            Once you understand how to access attributes of each order object, you need to find out where the closest
-            bin is to retrieve for the order
-            The next step after finding the nearest bin is obviously to move that bin from its location to the packer
-            associated with the order. Depending on whether you are using a robot to move bins to the packers or have 
-            a system built that transports the bins to the packers, your logic will differ slightly. 
-            Robot:
-                1. Move robot to closest bin
-                2. Move robot from closest bin to correct packer
-                3. Move bin back to open spot 
-            System:
-                1. Transport closest bin to correct packer
-            '''
-            """
-            The rest is for YOU to complete. Always be aware and make sure to update locations of objects when moving 
-            around the grid. Remember, you cannot bring a bin to a grid location that is already occupied by another
-            bin. You should create a variable that keeps track of the location of empty spots in your grid. Once you've 
-            completed an order be sure to call the order_complete() function on your cur_order and read the next order in.
-            """
+            ars.move_to_bin(cur_bin) #for now, assume we're taking bin back to its location
+
         else:
             sleep(1)
 
- 
+
 
 class ARS():
     def __init__(self, x, y):
-        #TODO: verify which motor is which
-        self.mA = LargeMotor('A') #motor:
-        self.mB = LargeMotor('B') #motor:
-        self.mC = LargeMotor('C') #motor:
+        self.mA = LargeMotor('outA') #motor: pulley motor
+        self.mB = LargeMotor('outB') #motor: wheel motor
+        self.mC = LargeMotor('outC') #motor: wheel motor
         self.x = x #SK - these are the initial coordinates of the ARS
         self.y = y
 
-'''
-In the ARS class you can define different functions to help your system retrieve inventory bins for
-each packer. Some examples to help get you started are below.
-'''
     def find_nearest_bin(self, bins, packer, color):
         dist = 20
-        for i in bins:
-            if bins[i].color == color:
+        near_bin = bins[0]
+        for this in bins:
+            if this.color == color:
                 # simply the total horizontal and vertical distance your system needs to travel depending on
                 # how you track locations in your system, distance calculation will differ
-                new_dist = abs(packer.x - bins[i].x)+abs(packer.y - bins[i].y)
+                new_dist = abs(packer.x - this.x)+abs(packer.y - this.y)
                 # makes sure that you are not trying to retrieve an empty bin
-                if new_dist < dist & bins[i].quantity > 0:
+                if new_dist < dist & this.quantity > 0:
                     dist = new_dist
-                    index = i
-        return(bins[index])
+                    near_bin = this
+        return near_bin
 
     def move_to_bin(self, bin):
-        # TODO
-        std_turn_X = #this is the number of degrees the pulley must turn to move one space 
+        print("In move_to_bin")
+        std_turn_X = 308.5157698 #this is the number of degrees the pulley must turn to move one space
         std_turn_Y = 358.098622 #this is the number of degrees the wheels are required to turn to move one space
         x_move = bin.x - self.x
         y_move = bin.y - self.y
-        
-        #TODO verify motors
-        mA.move(x_move*std_turn_X)
-        mB.move(y_move*std_turn_Y)
-        mC.move(y_move*std_turn_Y)
-        
+        self.mA.run_to_rel_pos(position_sp=x_move*std_turn_X, speed_sp=100, stop_action="brake")
+        self.mB.run_to_rel_pos(position_sp=y_move*std_turn_Y, speed_sp=100, stop_action="brake")
+        self.mC.run_to_rel_pos(position_sp=y_move*std_turn_Y, speed_sp=100, stop_action="brake")
         #!/usr/bin/env python3 - what's this? -SK
-        updateCurLocation(bin.x, bin.y)
-        
-       
-    
+        self.updateCurLocation(bin.x, bin.y)
+
+
+
     def move_to_packer(self, bin, packer):
-        # TODO
-        std_turn_X = #this is the number of degrees the pulley must turn to move one space 
-        std_turn_Y = 361.9114 #this is the number of degrees the wheels are required to turn to move one space
+        print("In move_to_packer")
+        std_turn_X = 308.5157698 #this is the number of degrees the pulley must turn to move one space
+        std_turn_Y = 358.098622 #this is the number of degrees the wheels are required to turn to move one space
         x_move = packer.x - self.x
+        print(x_move)
         y_move = packer.y - self.y
-        
-        #TODO verify motors
-        mA.move(x_move*std_turn_X)
-        mB.move(y_move*std_turn_Y)
-        mC.move(y_move*std_turn_Y)
-        
+        print(y_move)
+        self.mA.run_to_rel_pos(position_sp=x_move*std_turn_X, speed_sp=100, stop_action="brake")
+        self.mB.run_to_rel_pos(position_sp=y_move*std_turn_Y, speed_sp=100, stop_action="brake")
+        self.mC.run_to_rel_pos(position_sp=y_move*std_turn_Y, speed_sp=100, stop_action="brake")
         #!/usr/bin/env python3 - what's this? -SK
-        updateCurLocation(bin.x, bin.y)
-        
+        self.updateCurLocation(bin.x, bin.y)
+
 
     def updateCurLocation(self, x, y): #SK this is a method I’ve added
         self.x = x
         self.y = y
-        
-        
+
+
         
 
 '''
@@ -162,15 +127,6 @@ class Bin():
         self.quantity = 3
         self.replenish_cost = 0
 
-    #TODO: delete these methods if they're not used    
-    def getColor():
-        return color
-
-    def getX():
-        return x
- 
-    def getY():
-        return y
 
     # call this function when you have ran out of inventory
     def replenish(self):
